6c349a790aba156c7a8e923530ae8db8e2d2b5f3
ali9674 <ali_123-fb@hotmail.com>
2023-02-23 22:39:02 +0300

    Paint 2D
    2 Boyutlu şekil çizimi..

---
+++ Paint/Paint/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Paint
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aRAÇLARToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.temizleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.silgiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.silgiKalınlığıToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.araçlarToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.özelleştirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.seçeneklerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.comingSoonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(700, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(38, 32);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.comboBox1.Location = new System.Drawing.Point(561, 1);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(506, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kalınlık";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aRAÇLARToolStripMenuItem,
+            this.araçlarToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // aRAÇLARToolStripMenuItem
+            // 
+            this.aRAÇLARToolStripMenuItem.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.aRAÇLARToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.temizleToolStripMenuItem,
+            this.silgiToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.comingSoonToolStripMenuItem});
+            this.aRAÇLARToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("aRAÇLARToolStripMenuItem.Image")));
+            this.aRAÇLARToolStripMenuItem.Name = "aRAÇLARToolStripMenuItem";
+            this.aRAÇLARToolStripMenuItem.Size = new System.Drawing.Size(78, 24);
+            this.aRAÇLARToolStripMenuItem.Text = "Tools";
+            // 
+            // temizleToolStripMenuItem
+            // 
+            this.temizleToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("temizleToolStripMenuItem.Image")));
+            this.temizleToolStripMenuItem.Name = "temizleToolStripMenuItem";
+            this.temizleToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.temizleToolStripMenuItem.Text = "Temizle";
+            this.temizleToolStripMenuItem.Click += new System.EventHandler(this.temizleToolStripMenuItem_Click_1);
+            // 
+            // silgiToolStripMenuItem
+            // 
+            this.silgiToolStripMenuItem.CheckOnClick = true;
+            this.silgiToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.silgiKalınlığıToolStripMenuItem});
+            this.silgiToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("silgiToolStripMenuItem.Image")));
+            this.silgiToolStripMenuItem.Name = "silgiToolStripMenuItem";
+            this.silgiToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.silgiToolStripMenuItem.Text = "Silgi";
+            this.silgiToolStripMenuItem.Click += new System.EventHandler(this.silgiToolStripMenuItem_Click);
+            // 
+            // silgiKalınlığıToolStripMenuItem
+            // 
+            this.silgiKalınlığıToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBox1});
+            this.silgiKalınlığıToolStripMenuItem.Name = "silgiKalınlığıToolStripMenuItem";
+            this.silgiKalınlığıToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.silgiKalınlığıToolStripMenuItem.Text = "Silgi Kalınlığı";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 28);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(221, 6);
+            // 
+            // araçlarToolStripMenuItem1
+            // 
+            this.araçlarToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.özelleştirToolStripMenuItem,
+            this.seçeneklerToolStripMenuItem});
+            this.araçlarToolStripMenuItem1.Name = "araçlarToolStripMenuItem1";
+            this.araçlarToolStripMenuItem1.Size = new System.Drawing.Size(70, 24);
+            this.araçlarToolStripMenuItem1.Text = "&Araçlar";
+            // 
+            // özelleştirToolStripMenuItem
+            // 
+            this.özelleştirToolStripMenuItem.Name = "özelleştirToolStripMenuItem";
+            this.özelleştirToolStripMenuItem.Size = new System.Drawing.Size(163, 26);
+            this.özelleştirToolStripMenuItem.Text = "&Özelleştir";
+            // 
+            // seçeneklerToolStripMenuItem
+            // 
+            this.seçeneklerToolStripMenuItem.Name = "seçeneklerToolStripMenuItem";
+            this.seçeneklerToolStripMenuItem.Size = new System.Drawing.Size(163, 26);
+            this.seçeneklerToolStripMenuItem.Text = "&Seçenekler";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(458, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(42, 30);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(761, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(39, 30);
+            this.button2.TabIndex = 5;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // comingSoonToolStripMenuItem
+            // 
+            this.comingSoonToolStripMenuItem.Name = "comingSoonToolStripMenuItem";
+            this.comingSoonToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.comingSoonToolStripMenuItem.Text = "Coming Soon.....";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Paint 2D (v1)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown_1);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aRAÇLARToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem temizleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem silgiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem silgiKalınlığıToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem araçlarToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem özelleştirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem seçeneklerToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripMenuItem comingSoonToolStripMenuItem;
+    }
+}
+
---
+++ Paint/Paint/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Paint
+{
+    public partial class Form1 : Form
+    {
+        ColorDialog dlg = new ColorDialog();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        bool state;
+        int baslaX, baslaY;
+        int kalinlik = 5;
+
+       
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            state = false;
+
+        }
+
+        private void Form1_MouseDown_1(object sender, MouseEventArgs e)
+        {
+
+            state = true;
+            baslaX = e.X;
+            baslaY = e.Y;
+
+        }
+        bool silgidurum;
+        int silgiboyut;
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            silgidurum = false;
+            Graphics g = this.CreateGraphics();
+            
+            Pen p = new Pen(dlg.Color, kalinlik);
+            Pen silgi = new Pen(Form1.DefaultBackColor, silgiboyut);
+            Point p1 = new Point(baslaX, baslaY);
+            Point p2 = new Point(e.X, e.Y);
+
+            if (silgiToolStripMenuItem.Checked == true)
+            {
+                g.DrawLine(silgi, p1, p2);
+                baslaX = e.X;
+                baslaY = e.Y;
+                silgidurum = true;  
+            }
+
+
+
+
+
+
+            if (state == true &&silgidurum==false)
+            {
+                g.DrawLine(p, p1, p2);
+                baslaX = e.X;
+                baslaY = e.Y;
+
+            }
+
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            kalinlik = int.Parse(comboBox1.Text);
+        }
+
+      
+
+        private void temizleToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            g.Clear(Form1.DefaultBackColor);
+        }
+
+        private void silgiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            object[] kalınlık = { "10", "20", "30", "40", "50", "60" };
+            toolStripComboBox1.Items.AddRange(kalınlık);
+            toolStripComboBox1.Text = "10";
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            button2.BackColor = dlg.Color;
+
+            silgiboyut = int.Parse(toolStripComboBox1.Text);    
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            dlg.ShowDialog();
+        }
+    }
+}
